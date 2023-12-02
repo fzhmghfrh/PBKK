621cb5a0757ea6b9b58eb2b30ef09badf829635e
fzhmghfrh <faizahmaghfirah@gmail.com>
2023-12-02 12:03:07 +0700

    tugas 10 & 12

---
+++ 12.CRUDCI/CI/app/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        echo view('/inc/header');
+        echo view('home');
+        echo view('/inc/footer');
+    }
+}
